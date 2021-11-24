3bf5e1e3599b739b3ebebfd4890ad2fa5d2a29fb
Jens Krumsieck <jenso666@live.de>
2021-11-24 11:37:08 +0100

    🧹 cleanup

--- ChemSharp.Molecules.Blazor/Extension/JSInteropUtil.cs
+++ ChemSharp.Molecules.Blazor/Extension/JSInteropUtil.cs
@@ -10,7 +10,7 @@ namespace ChemSharp.Molecules.Blazor.Extension
         /// <param name="JS"></param>
         /// <param name="querySelector"></param>
         /// <returns></returns>
-        public static async Task InitThreeJSAsync(this IJSRuntime JS, string querySelector = "three-webgl") 
+        public static async Task InitThreeJSAsync(this IJSRuntime JS, string querySelector = "three-webgl")
             => await JS.InvokeVoidAsync("chemsharpMolecules.init", querySelector);
 
         public static async Task ClearCanvas(this IJSRuntime JS) => await JS.InvokeVoidAsync("chemsharpMolecules.clearCanvas");
