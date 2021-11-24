16ed0d4fd8354f8c849b9c08a268f0ac4436f6f0
Jens Krumsieck <jenso666@live.de>
2021-11-24 11:34:58 +0100

    Move Components

---
+++ ChemSharp.Molecules.Blazor/BlazorAtom.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace ChemSharp.Molecules.Blazor
+{
+    public class BlazorAtom : ComponentBase
+    {
+        [Parameter]
+        public Atom? Data { get; set; }
+
+        [Inject]
+        public IJSRuntime JS { get; set; }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (Data == null) return;
+            await JS.InvokeVoidAsync("chemsharpMolecules.addAtom", Data.Title, Data.Symbol, Data.Location.X, Data.Location.Y, Data.Location.Z, Data.CovalentRadius ?? 100, Data.Color);
+        }
+    }
+}
---
+++ ChemSharp.Molecules.Blazor/BlazorBond.cs
@@ -0,0 +1,21 @@
+﻿using BlazorThreeJS.Extension;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace ChemSharp.Molecules.Blazor
+{
+    public class BlazorBond : ComponentBase
+    {
+        [Parameter]
+        public Bond? Data { get; set; }
+
+        [Inject]
+        public IJSRuntime JS { get; set; }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (Data == null) return;
+            var (loc, matrix) = Data.CalculateRotation();
+            await JS.InvokeVoidAsync("chemsharpMolecules.addBond", loc.X, loc.Y, loc.Z, matrix.X, matrix.Y, matrix.Z, matrix.W, Data.Length);
+        }
+    }
+}
