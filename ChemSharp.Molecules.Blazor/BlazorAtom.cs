9ce712fee27c7ed2a2796684500a4b7c97617ea8
Jens Krumsieck <jenso666@live.de>
2021-11-25 10:48:27 +0100

    ⚛️ Build Molecule Component

--- ChemSharp.Molecules.Blazor/BlazorAtom.cs
+++ ChemSharp.Molecules.Blazor/BlazorAtom.cs
@@ -6,14 +6,14 @@ namespace ChemSharp.Molecules.Blazor
     public class BlazorAtom : ComponentBase
     {
         [Parameter]
-        public Atom? Data { get; set; }
+        public Atom? Atom { get; set; }
 
         [Inject]
         public IJSRuntime JS { get; set; }
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            if (Data == null) return;
-            await JS.InvokeVoidAsync("chemsharpMolecules.addAtom", Data.Title, Data.Symbol, Data.Location.X, Data.Location.Y, Data.Location.Z, Data.CovalentRadius ?? 100, Data.Color);
+            if (Atom == null) return;
+            await JS.InvokeVoidAsync("chemsharpMolecules.addAtom", Atom.Title, Atom.Symbol, Atom.Location.X, Atom.Location.Y, Atom.Location.Z, Atom.CovalentRadius ?? 100, Atom.Color);
         }
     }
 }
--- ChemSharp.Molecules.Blazor/BlazorBond.cs
+++ ChemSharp.Molecules.Blazor/BlazorBond.cs
@@ -7,15 +7,15 @@ namespace ChemSharp.Molecules.Blazor
     public class BlazorBond : ComponentBase
     {
         [Parameter]
-        public Bond? Data { get; set; }
+        public Bond? Bond { get; set; }
 
         [Inject]
         public IJSRuntime JS { get; set; }
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            if (Data == null) return;
-            var (loc, matrix) = Data.CalculateRotation();
-            await JS.InvokeVoidAsync("chemsharpMolecules.addBond", loc.X, loc.Y, loc.Z, matrix.X, matrix.Y, matrix.Z, matrix.W, Data.Length);
+            if (Bond == null) return;
+            var (loc, matrix) = Bond.CalculateRotation();
+            await JS.InvokeVoidAsync("chemsharpMolecules.addBond", loc.X, loc.Y, loc.Z, matrix.X, matrix.Y, matrix.Z, matrix.W, Bond.Length);
         }
     }
 }
