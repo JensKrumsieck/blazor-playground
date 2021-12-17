59b94db70db43744e1a4d645f57402272c2acafd
Jens Krumsieck <jenso666@live.de>
2021-12-17 10:41:14 +0100

    Fix Scaling, add Tracker

--- OxyPlot.SkiaSharp.Blazor/PlotView.cs
+++ OxyPlot.SkiaSharp.Blazor/PlotView.cs
@@ -18,6 +18,7 @@ public class PlotView : ComponentBase, IPlotView
     private ElementReference _wrapper;
     private SKSize _canvasSize;
     private SizeWatcherInterop sizeWatcher = null!;
+    private TrackerHitResult _lastTrackerHitResult;
 
     #region Parameters
     [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object> UnmatchedParameters { get; set; }
@@ -51,13 +52,26 @@ public class PlotView : ComponentBase, IPlotView
     /// <inheritdoc/>
     IController IView.ActualController => ActualController;
 
-    public OxyRect ClientArea => new(0, 0, _canvasSize.Width, _canvasSize.Height);
+    public OxyRect ClientArea => CalculateBounds();
     /// <inheritdoc/>
-    public void HideTracker() { }
+    public void HideTracker()
+    {
+        _lastTrackerHitResult = null!;
+        StateHasChanged();
+    }
     /// <inheritdoc/>
     public void HideZoomRectangle() { }
     /// <inheritdoc/>
-    public void ShowTracker(TrackerHitResult trackerHitResult) { }
+    public void ShowTracker(TrackerHitResult trackerHitResult)
+    {
+        if (trackerHitResult == null)
+        {
+            HideTracker();
+            return;
+        }
+        _lastTrackerHitResult = trackerHitResult;
+        StateHasChanged();
+    }
     /// <inheritdoc/>
     public void ShowZoomRectangle(OxyRect rectangle) { }
 
@@ -150,9 +164,16 @@ public class PlotView : ComponentBase, IPlotView
     {
         ClearBackground();
         if (ActualModel == null) return;
+        lock (ActualModel.SyncRoot) ((IPlotModel)ActualModel).Render(_renderContext, CalculateBounds());
+    }
+
+    private OxyRect CalculateBounds()
+    {
+        //TODO: Better solution: https://github.com/mono/SkiaSharp/pull/1832
         var dpiFi = typeof(SKCanvasView).GetField("dpi", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
         var dpi = (double)dpiFi.GetValue(_canvasView);
-        lock (ActualModel.SyncRoot) ((IPlotModel)ActualModel).Render(_renderContext, new OxyRect(0, 0, (int)(_canvasSize.Width * dpi), (int)(_canvasSize.Height * dpi)));
+        SKRenderContext.DpiScale = (float)dpi;
+        return new OxyRect(0, 0, (int)(_canvasSize.Width), (int)(_canvasSize.Height));
     }
     protected void ClearBackground()
     {
@@ -186,7 +207,11 @@ public class PlotView : ComponentBase, IPlotView
         sizeWatcher = await SizeWatcherInterop.ImportAsync(JS, _wrapper, OnSizeChanged);
     }
 
-    private void OnSizeChanged(SKSize size) => _canvasSize = size;
+    private void OnSizeChanged(SKSize size)
+    {
+        _canvasSize = size;
+        StateHasChanged();
+    }
 
     /// <summary>
     /// Adds SKCanvasElement to RenderTree
@@ -196,10 +221,11 @@ public class PlotView : ComponentBase, IPlotView
     {
         builder.OpenElement(0, "div");
         builder.AddMultipleAttributes(1, UnmatchedParameters);
+        builder.AddAttribute(1, "class", $"oxyplotview {(UnmatchedParameters.ContainsKey("class") ? UnmatchedParameters["class"] : "")}");
+        builder.AddAttribute(1, "style", $"position: relative; {(UnmatchedParameters.ContainsKey("style") ? UnmatchedParameters["style"] : "")}");
         builder.OpenComponent<SKCanvasView>(1);
         builder.AddAttribute(2, "OnPaintSurface", OnPaintSurface);
-        builder.AddAttribute(2, "style", "width: 100%; height: inherit;");
-
+        builder.AddAttribute(2, "style", "width: 100%; height: inherit;"); //do not override!
         AddEventCallback<MouseEventArgs>(builder, 3, "onmousedown", e => ActualController.HandleMouseDown(this, e.OxyMouseEventArgs()));
         AddEventCallback<MouseEventArgs>(builder, 3, "onmousemove", e => ActualController.HandleMouseMove(this, e.OxyMouseEventArgs()));
         AddEventCallback<MouseEventArgs>(builder, 3, "onmouseup", e => ActualController.HandleMouseUp(this, e.OxyMouseEventArgs()));
@@ -216,6 +242,16 @@ public class PlotView : ComponentBase, IPlotView
         builder.AddComponentReferenceCapture(6, reference => _canvasView = (SKCanvasView)reference);
         builder.CloseComponent();
         builder.AddElementReferenceCapture(6, reference => _wrapper = reference);
+
+        if (_lastTrackerHitResult != null)
+        {
+            builder.OpenElement(7, "div");
+            builder.AddAttribute(7, "class", "oxytracker");
+            builder.AddAttribute(7, "style", $"position: absolute; left: {(int)_lastTrackerHitResult.Position.X}px; top: {(int)_lastTrackerHitResult.Position.Y}px");
+            builder.AddContent(8, (MarkupString)_lastTrackerHitResult.Text);
+            builder.CloseElement();
+        }
+
         builder.CloseElement();
     }
     #endregion
